3028a75cee8b424fac2393ebab25d4f12fdf15d4
lucas-deoliveira <eng.oliveiralucas@hotmail.com>
2020-05-07 19:34:42 -0300

    repository

---
+++ src/main/java/br/usjt/Repository/PrevisoesDoTempoRepository.java
@@ -0,0 +1,9 @@
+package br.usjt.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.usjt.model.PrevisoesDoTempo;
+
+	public interface PrevisoesDoTempoRepository extends JpaRepository<PrevisoesDoTempo, Long>{	
+
+}
