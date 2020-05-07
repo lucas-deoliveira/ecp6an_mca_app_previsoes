41f6e3af22e0bc43ae73ce0e468c113cc1a6bf51
lucas-deoliveira <eng.oliveiralucas@hotmail.com>
2020-05-07 19:31:43 -0300

    import.sql

---
+++ src/main/resources/import.sql
@@ -0,0 +1,11 @@
+
+insert into previsoes_do_tempo(id, data, semana, temp_min, temp_max, umi, descricao, la, lo) values(1, '2020-04-26 12:00:00', 'Domingo' , 20.9 , 26.2 , 55 , 'Céu limpo com poucas nuvens', -20.0000, 20.0000);
+insert into previsoes_do_tempo(id, data, semana, temp_min, temp_max, umi, descricao, la, lo) values(2, '2020-04-27 14:00:00', 'Segunda-feira' , 17.3 , 20.0 , 46 , 'Sol', -20.0000, 20.0000);
+insert into previsoes_do_tempo(id, data, semana, temp_min, temp_max, umi, descricao, la, lo) values(3, '2020-04-28 22:00:00', 'Terça-feira' , 21.5 , 24.4 , 60 , 'Parcialmente nublado', -20.0000, 20.0000);
+insert into previsoes_do_tempo(id, data, semana, temp_min, temp_max, umi, descricao, la, lo) values(4, '2020-04-29 20:00:00', 'Quarta-feira' , 19.9 , 20.0 , 62 , 'Sol', -20.0000, 20.0000);
+insert into previsoes_do_tempo(id, data, semana, temp_min, temp_Max, umi, descricao, la, lo) values(5, '2020-04-30 17:00:00', 'Quinta-feira' , 18.8 , 21.5 , 75 , 'Sol com algumas nuvens. Não chove.', 20.0000, -20.0000);
+insert into previsoes_do_tempo(id, data, semana, temp_min, temp_max, umi, descricao, la, lo) values(6, '2020-05-01 12:00:00', 'Sexta-feira' , 17.5 , 19.0 , 64 , 'Parcialmente nublado', 20.0000, -20.0000);
+insert into previsoes_do_tempo(id, data, semana, temp_min, temp_max, umi, descricao, la, lo) values(7, '2020-05-01 19:00:00', 'Sabado' , 19.2 , 22.5 , 67 , 'Céu Nublado', 20.0000, -20.0000);
+
+insert into usuario(id, login, senha) values (1, 'sysadmin', 'admin')
+insert into usuario(id, login, senha) values (2, 'aluno', 'aluno@123')
